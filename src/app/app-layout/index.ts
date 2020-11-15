ca255dbbfc1299c07636470886a351b77f252ca2
Dilshat Hewzulla <hewzulla@gmail.com>
2020-11-15 00:16:42 +0000

    added useWindowResize

--- src/app/app-layout/index.ts
+++ src/app/app-layout/index.ts
@@ -4,10 +4,11 @@ import SelectableInput from './selectable-input';
 import InputWithCopy from './input-with-copy';
 import PageContainer from './page-container';
 import DisplayCanvas from './display-canvas';
+import useWindowSize from './use-window-size';
 
 import {SelectionContainer, RadioButton,CheckboxButton} from './selectable';
 
 export * from './page-elements';
 export {InputWithLabel,TextButton,SelectableInput,InputWithCopy};
 export {SelectionContainer, RadioButton,CheckboxButton};
-export {PageContainer,DisplayCanvas};
+export {PageContainer,DisplayCanvas,useWindowSize};
