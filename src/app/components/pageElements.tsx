c05ee7163542d1d279302e1a8bf936155e893131
Dilshat Hewzulla <hewzulla@gmail.com>
2021-01-04 21:08:07 +0000

    added-common-library

---
+++ src/app/components/pageElements.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Field,SourceLinkElement,AppContainerElement,AppTitleSection,AppTitle,AppBody,AppContent} from '../common-elements';
+
+
+const SourceLink=styled(SourceLinkElement).attrs({
+    href:'https://github.com/global-input/game-control-example',
+    rel:'noreferrer noopener',
+    target:'_blank'})``;
+
+
+    export  const AppContainer:React.FC=({children})=>(
+        <AppContainerElement>
+                <AppTitleSection>
+                    <AppTitle>Form Data Transfer</AppTitle>
+                </AppTitleSection>
+                <AppBody>
+                    <AppContent>
+                    {children}
+                    </AppContent>
+                    <SourceLink>Source Code</SourceLink>
+                </AppBody>
+        </AppContainerElement>
+    );
+    export const DomainField=styled(Field)`
+        max-width:300px;
+
+    `;
